6643ce0b0144262290ab293085ef5a08654c2d79
Jon Sagotsky <sagotsky@gmail.com>
2018-01-20 21:06:54 -0500

    Swapped in ImageCollections model for finding images.

---
+++ lib/image_collection.rb
@@ -0,0 +1,41 @@
+class ImageCollection
+  def initialize(path, resolutions_hash)
+    @path = path
+    @resolutions_hash = resolutions_hash
+  end
+
+  # returns
+  # [{thumb: path/to/thumb/01.jpg, full: path/to/full/01.jpg}]
+  def group_by_resolution
+    {}.tap do |image_hashes|
+      resolution_labels.map do |label|
+        images_by_filename(label).each do |filename, filepath|
+          image_hashes[filename] ||= {}
+          image_hashes[filename][label] = filepath
+        end
+      end
+    end.values # would the filename key be valuable?
+  end
+
+  private
+
+  def resolution_labels
+    @resolutions_hash.keys
+  end
+
+  def images_by_filename(label)
+    images(label).each_with_object({}) do |image, hash|
+      hash[filename(image)] = image
+    end
+  end
+
+  def images(label)
+    Dir["public/#{@path}/#{label}/*"].sort.map do |f|
+      f.gsub %r{^public\/}, ''
+    end
+  end
+
+  def filename(filepath)
+    filepath.split('/').last
+  end
+end
--- server.rb
+++ server.rb
@@ -10,16 +10,25 @@ Bundler.require
 #   enable :logging, :dump_errors, :raise_errors
 # end
 
+module ImageCollections
+  def self.gallery
+    ImageCollection.new('images/gallery', {thumb: 250, full: 2000})
+  end
+
+  def self.carousel
+    ImageCollection.new('images/carousel', {thumb: 740, full: 2000})
+  end
+end
 
 get '/' do
-  @carousel_images = ImageFinder.new('images/carousel', 740, 2000).thumbs_and_full
+  @carousel_images = ImageCollections.carousel.group_by_resolution
   @body_class = 'home'
 
   erb :index
 end
 
 get '/gallery' do
-  @gallery_images = ImageFinder.new('images/gallery', 250, 2000).thumbs_and_full
+  @gallery_images = ImageCollections.gallery.group_by_resolution
   @body_class = 'gallery'
 
   erb :gallery
@@ -31,20 +40,43 @@ get '/about' do
   erb :about
 end
 
-get '/test' do
-  output = "before: #{Time.now}"
-  ImageUpdateManager.new('inbox/', '_resized').update
-  output << "after: #{Time.now}"
+# get '/test' do
+#   output = "before: #{Time.now}"
+#   ImageUpdateManager.new('inbox/', '_resized').update
+#   output << "after: #{Time.now}"
 
-  output
-end
+#   output
+# end
 
 # list all images in inbox and whether they're converted.  maybe the timestamp isn't the best idea?
+# maybe that page could post the dest each time?
+# or maybe it could loop over originals and skip items that were converted?
 get '/admin' do
-  @image_inbox = ImageInbox.new
+  @image_inbox = ImageInbox.new('inbox/gallery', '_resized/gallery', 250, 2000)
 
   erb :admin
 end
+
+get '/admin/image-inbox.json' do
+  content_type :json
+
+  @gallery_inbox = ImageInbox.new('inbox/gallery', '_resized/gallery', 250, 2000)
+  @carousel_inbox = ImageInbox.new('inbox/carousel', '_resized/carousel', 740, 2000)
+  binding.pry
+
+  @gallery_inbox.ls.each_with_object({}) do |filename, done|
+
+  end
+
+  {}.to_json
+end
+
+post '/admin/resize-image.json' do
+  content_type :json
+
+  {status: :ok}.to_json
+end
+#
 
 __END__
 get '/hello-world.json' do
@@ -54,21 +86,15 @@ get '/hello-world.json' do
   {greeting: 'Hello World!'}.to_json
 end
 
+TODO
 
-# existing pages
-get '/index' do
-end
+* Admin page
+** List of files.  Ajaxily convert them.  Waiting 10 min without ajax is too much
 
-get '/home' do
-end
+* FsReader
+** Some sort of file system reader.  Give it a subdir, queyr it to get files out
 
-get '/about' do
-end
-
-get '/admin-update' do
-  # can we resize images on dh?
-end
-
-get '/status' do
-  # see status of image conversion.  maybe we can have an import directory and a resized dir.  magick one image at a time, move when done
-end
+* ImageCollection
+** Instead of finding images in subdirs by resolution, an image collection has a dir for thumbs and full.  Maybe that's overridable.
+** @gallery = ImageCollection.new('images/gallery', thumb: 250, full: 2000)
+** maybe the collection knows about originals too?
